bb5e677977a1b04092e4a2b0521e5521350a1033
yingying kang <leroytangyl@gmail.com>
2020-10-14 14:07:41 +0800

    Initial Commit

---
+++ DownloadCSV/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+
+namespace DownloadCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = @"https://docs.google.com/spreadsheets/d/1OZS6mNqsjTfoxpHpbIYOUEmISALj1nBvFJ7Zxsk5VgY/edit?usp=sharing&output=csv";
+
+            WebClientEx wc = new WebClientEx(new CookieContainer());
+            wc.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0");
+            wc.Headers.Add("DNT", "1");
+            wc.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            wc.Headers.Add("Accept-Encoding", "deflate");
+            wc.Headers.Add("Accept-Language", "en-US,en;q=0.5");
+
+            byte[] dt = wc.DownloadData(url);
+            var outputCSVdata = System.Text.Encoding.UTF8.GetString(dt ?? new byte[] { });
+            Console.WriteLine(outputCSVdata);
+        }
+        public class WebClientEx : WebClient
+        {
+            public WebClientEx(CookieContainer container)
+            {
+                this.container = container;
+            }
+
+            private readonly CookieContainer container = new CookieContainer();
+
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                WebRequest r = base.GetWebRequest(address);
+                var request = r as HttpWebRequest;
+                if (request != null)
+                {
+                    request.CookieContainer = container;
+                }
+                return r;
+            }
+
+            protected override WebResponse GetWebResponse(WebRequest request, IAsyncResult result)
+            {
+                WebResponse response = base.GetWebResponse(request, result);
+                ReadCookies(response);
+                return response;
+            }
+
+            protected override WebResponse GetWebResponse(WebRequest request)
+            {
+                WebResponse response = base.GetWebResponse(request);
+                ReadCookies(response);
+                return response;
+            }
+
+            private void ReadCookies(WebResponse r)
+            {
+                var response = r as HttpWebResponse;
+                if (response != null)
+                {
+                    CookieCollection cookies = response.Cookies;
+                    container.Add(cookies);
+                }
+            }
+        }
+    }
+}
